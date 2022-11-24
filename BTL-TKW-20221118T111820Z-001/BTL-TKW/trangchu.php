0223e214f8ad51733647dcf3129a26e3c3995f4f
hvdhavietduc <duc.ha000412@hcmut.edu.vn>
2022-11-24 21:47:26 +0700

    newupdate

--- BTL-TKW-20221118T111820Z-001/BTL-TKW/trangchu.php
+++ BTL-TKW-20221118T111820Z-001/BTL-TKW/trangchu.php
@@ -5,14 +5,14 @@
     <title>Male_Shoe</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/listStyles.css" />
+    <link rel="stylesheet" href="listStyles.css" />
 </head>
 
 <body>
 
     <?php
     include "./connect_db.php";
-    $item_per_page = !empty($_GET["per_page"]) ? $_GET["per_page"] : 8;
+    $item_per_page = !empty($_GET["per_page"]) ? $_GET["per_page"] : 16;
     $current_page = !empty($_GET["page"]) ? $_GET["page"] : 1;
     $offset = ($current_page - 1) * $item_per_page;
     $products = mysqli_query(
@@ -26,20 +26,18 @@
     $totalRecords = $totalRecords->num_rows;
     $totalPages = ceil($totalRecords / $item_per_page);
     ?>
-
+    <div class="imgheader">
+            <img src="img/imgheader.png">
+    </div>
     <div class="container">
         <h1>Danh sách sản phẩm</h1>
         <div class="product-items">
             <?php while ($row = mysqli_fetch_array($products)) { ?>
             <div class="product-item">
                 <div class="product-img">
-                    <img src="<?= $row["image"] ?>" title="<?= $row[
-  "name"
-] ?>" />
+                    <a href="detailpro.php?id=<?= $row["id"] ?>"><img src="<?= $row["image"] ?>" title="<?= $row["name"] ?>" ></img></a>
                 </div>
-                <strong><a href="detail.php?id=<?= $row["id"] ?>"><?= $row[
-  "name"
-] ?></a></strong><br />
+                <strong><a href="detailpro.php?id=<?= $row["id"] ?>"><?= $row["name"] ?></a></strong><br />
                 <label>Giá: </label><span class="product-price"><?= number_format(
                   $row["price"],
                   0,
@@ -47,7 +45,7 @@
                   "."
                 ) ?> đ</span><br />
                 <div class="buy-button">
-                    <a href="detail.php?id=<?= $row["id"] ?>">Mua sản phẩm</a>
+                    <a href="detailpro.php?id=<?= $row["id"] ?>">Mua sản phẩm</a>
                 </div>
             </div>
             <?php } ?>
